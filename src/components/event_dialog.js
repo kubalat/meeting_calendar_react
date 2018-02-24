1e0105b2ff02147f0d650a3f1c19a6e2e3c284fd
kubalat <kubala.tomas@hotmail.com>
2018-02-24 18:10:27 +0100

    View form for event dialog

--- src/components/event_dialog.js
+++ src/components/event_dialog.js
@@ -11,6 +11,31 @@ export default class EventDialog extends Component {
             left: this.props.positionX
         }
 
-        return (<div className="event__dialog" style={ dialogStyle }>Test</div>);
+        return (
+            <div className="event__dialog" style={ dialogStyle }>
+                <header>
+                    <div className="header__title"><h3>Event name</h3></div>
+                    <div className="header__close"><i onClick={ () => this.closeDialog() }class="far fa-times-circle"></i></div>
+                </header>
+                <div class="event__body">
+                    <div className="event__metadata" id="event__time">
+                        <div className="meta__icon"><i class="far fa-clock"></i></div>
+                        <div className="meta__value">8:00 - 9:00</div>
+                    </div>
+                    <div className="event__metadata" id="event__user">
+                        <div className="meta__icon"><i class="far fa-user"></i></div>
+                        <div className="meta__value">Tomáš Kubala</div>
+                    </div>
+                    <div className="event__metadata" id="event__note">
+                        <div className="meta__icon"><i class="far fa-sticky-note"></i></div>
+                        <div className="meta__value">This is a very big note. Olalalalalalalalala</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    closeDialog() {
+        this.setState({ isModalOpen: false});
     }
 }
